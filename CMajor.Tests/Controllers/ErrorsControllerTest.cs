fd741790d5674bb7e85064b76c500e9cea7cac2b
jittuu <soemoetunlwin@gmail.com>
2011-10-25 06:34:22 +0800

    lets handle 404 correctly

---
+++ CMajor.Tests/Controllers/ErrorsControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using CMajor.Controllers;
+using Xunit;
+
+namespace CMajor.Tests.Controllers {
+    public class ErrorsControllerTest {
+        [Fact]
+        public void Test_Errors_NotFound_should_return_ViewResult_with_404_view() {
+            var controller = new ErrorsController();
+            var result = controller.NotFound();
+
+            Assert.IsType<ViewResult>(result);
+
+            var viewResult = result as ViewResult;
+            Assert.Equal("404", viewResult.ViewName);
+        }
+    }
+}
---
+++ CMajor/Controllers/ErrorsController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace CMajor.Controllers {
+    public class ErrorsController : ApplicationController {
+        public ActionResult NotFound() {
+            return View("404");
+        }
+    }
+}
