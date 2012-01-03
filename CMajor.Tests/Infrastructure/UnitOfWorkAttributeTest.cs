846c62883e9b3e2b3f74d6ff9dc976b39141d145
jittuu <soemoetunlwin@gmail.com>
2012-01-03 11:30:49 +0800

    added Unit tests for UnitOfWorkAttribute

---
+++ CMajor.Tests/Infrastructure/UnitOfWorkAttributeTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using CMajor.Infrastructure;
+using Moq;
+using System.Web.Mvc;
+
+namespace CMajor.Tests.Infrastructure {
+    public class UnitOfWorkAttributeTest {
+        [Fact]
+        public void Test_Should_Commit_if_there_is_no_exception() {
+            var uowAttr = new UnitOfWorkAttribute();
+            var mockUoW = new Mock<IUnitOfWork>();
+            mockUoW.Setup(u => u.Commit()).Verifiable();
+
+            uowAttr.UnitOfWork = mockUoW.Object;
+            uowAttr.OnActionExecuted(new ActionExecutedContext());
+
+            mockUoW.Verify();
+        }
+
+        [Fact]
+        public void Test_Should_not_commit_if_there_is_Exception() {
+            var uowAttr = new UnitOfWorkAttribute();
+            var mockUoW = new Mock<IUnitOfWork>();
+            mockUoW.Setup(u => u.Commit()).Throws<InvalidOperationException>();
+
+            uowAttr.UnitOfWork = mockUoW.Object;
+            uowAttr.OnActionExecuted(new ActionExecutedContext() { Exception = new Exception() });
+        }
+    }
+}
